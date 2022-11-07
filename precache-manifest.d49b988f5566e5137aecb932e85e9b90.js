21e509f77465243cdfd08bae89f102f825739ccd
Pogoretsky Dmytro <pogoretsky.d@gmail.com>
2022-11-07 14:33:35 +0200

    Updates

---
+++ precache-manifest.d49b988f5566e5137aecb932e85e9b90.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdf9649740aacb45ba1930678c2468f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7404417a82f753534804",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "10fb56da6f610242ba1c",
+    "url": "./static/css/main.027c596a.chunk.css"
+  },
+  {
+    "revision": "7404417a82f753534804",
+    "url": "./static/js/2.582e6190.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "./static/js/2.582e6190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fb56da6f610242ba1c",
+    "url": "./static/js/main.151aae29.chunk.js"
+  },
+  {
+    "revision": "89c32e81c7e94021b924",
+    "url": "./static/js/runtime-main.5161ae7a.js"
+  },
+  {
+    "revision": "16d89ffee7a3d0e733aac9d194017cbf",
+    "url": "./static/media/Events.16d89ffe.svg"
+  },
+  {
+    "revision": "1f208b6773683be7d71bb1281178f8c8",
+    "url": "./static/media/Feed.1f208b67.svg"
+  },
+  {
+    "revision": "23f14537238f0f2d0a84a4a6073056f4",
+    "url": "./static/media/Home.23f14537.svg"
+  },
+  {
+    "revision": "674719e441d5bfdfd6b1c5063ea2a63c",
+    "url": "./static/media/Search.674719e4.svg"
+  },
+  {
+    "revision": "56f9ad1222bc47b4f08946a63cd17482",
+    "url": "./static/media/feed.56f9ad12.png"
+  },
+  {
+    "revision": "ded64069af04d511398d22a914d1f28a",
+    "url": "./static/media/feed2.ded64069.png"
+  },
+  {
+    "revision": "b54032140f5c6cf8f74ae8b8c50f3620",
+    "url": "./static/media/feed3.b5403214.png"
+  },
+  {
+    "revision": "ded64069af04d511398d22a914d1f28a",
+    "url": "./static/media/feed4.ded64069.png"
+  },
+  {
+    "revision": "b54032140f5c6cf8f74ae8b8c50f3620",
+    "url": "./static/media/feed5.b5403214.png"
+  },
+  {
+    "revision": "dcc086dbacac51355a08ce6c7c784578",
+    "url": "./static/media/home.dcc086db.png"
+  },
+  {
+    "revision": "4f6aeae1150fdd70bdfd92595ebc6723",
+    "url": "./static/media/home2.4f6aeae1.png"
+  },
+  {
+    "revision": "e7f7c822bc2dbee98efda2dee98d1785",
+    "url": "./static/media/home3.e7f7c822.png"
+  },
+  {
+    "revision": "556c36f6504e9e662eb8dc0d1004a383",
+    "url": "./static/media/home4.556c36f6.png"
+  }
+]);
